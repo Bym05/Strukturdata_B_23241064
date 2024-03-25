f65d12090fc595ea46856fa5d212b452a525bf20
Bym05 <146050438+Bym05@users.noreply.github.com>
2024-03-25 14:20:07 +0800

    25-03-2024

---
+++ MUHAMMAD BIMA YULIYANTO/meet1.py
@@ -0,0 +1,5 @@
+#Perkenalan Diri Mata Kuliah StrukturData
+print("NAMA SAYA BIMA/HIDA") #saya sangat ceria
+print("NIM 23241064")
+print("Kami Adalah Mahasiswa UNDIKMA PRODI PTI")
+#Saya sangat ceria
