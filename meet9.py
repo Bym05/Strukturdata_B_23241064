a8d53e42b55024e2d438e27be19bbcf688816b64
Bym05 <146050438+Bym05@users.noreply.github.com>
2024-06-03 11:45:06 +0800

    03-06-2024

---
+++ meet9.py
@@ -0,0 +1,22 @@
+# percabangan jika maka
+name = input("what is your name:")
+print (name)
+
+# if kondisi : aksi
+# if standar
+# if name == "edy" : print(dosen ganteng)
+# print("end of program")
+
+# 2. if identasi
+if name=="edy":
+    print("dosen ganteng")
+    print("ganteng banget")
+print("end of program")
+
+# 3. IF ELSE
+if name=="edy":
+    print("dosen ganteng")
+    print(f"terimakasih {name}")
+else:
+    print("anda tidak ganteng")
+print("end of program")
