79178eaf803ec22323081ab789eaf53c3f300032
Bym05 <146050438+Bym05@users.noreply.github.com>
2024-06-24 12:08:47 +0800

    10 Juni 2024

---
+++ meet10.py
@@ -0,0 +1,21 @@
+#Membuat percabangan yang lebih dari 1
+nama = input("masukkan nama :")
+
+if nama   =="Adam":
+    print ("Mahasiswa PTI B")
+elif nama =="Chelly":
+    print ("Mahasiswa PTI B")
+elif nama == "Ridho":
+    print ("Mahasiswa PTI B")
+elif nama == "Bima":
+    print("Mahasiswa PTI B")
+elif nama == "Budi":
+    print ("Mahasiswa PTI B")
+elif nama == "udin":
+    print("Mahasiswa Kehutanan")
+elif nama =="Adin":
+    print("Mahasiswa PTI B")
+
+else:
+    print("Bukan Mahasiswa PTI B")
+print("Program selesai")
